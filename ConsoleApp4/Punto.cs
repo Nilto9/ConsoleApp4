97998743f0399cbef604ca77f655cf9f4e656397
Nilton <palian.nilton@hotmail.com>
2022-03-30 17:30:05 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp4/Punto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{ 
+    public class Punto
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public Punto(int y, int x)
+        {
+            this.y = y;
+            this.x = x;
+        }
+    }
+}
---
+++ ConsoleApp4/Rectangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Rectangulo
+    {
+        public Punto p1 { get; set; }
+        public Punto p2 { get; set; }
+        public Punto p3 { get; set; }
+        public Punto p4 { get; set; }
+
+        public int CalcularArea()
+        {
+            int Area = (p3.y - p1.y) * (p2.x - p1.x);
+            return (Area);
+        }
+        public int CalcularPerimetro()
+        {
+            int peri = Math.Abs(((p3.y + p1.y) * 2) + ((p2.x + p1.x) * 2));
+            return peri;
+        }
+    }
+}
